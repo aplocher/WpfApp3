01b252819efb90849fb9b1c833535acc1f980384
Adam Plocher <aplocher@varis1.com>
2020-04-21 10:47:22 -0700

    Cleanup

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -1,23 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp3
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -35,19 +20,18 @@ namespace WpfApp3
         }
     }
 
-
-public class TestViewModel
-{
-    private string testProp;
-
-    public string TestProp
+    public class TestViewModel
     {
-        get => testProp;
-        set
+        private string testProp;
+
+        public string TestProp
         {
-            testProp = value;
-            throw new Exception("Test Exceptionnnn");
+            get => testProp;
+            set
+            {
+                testProp = value;
+                throw new Exception("Test Exception");
+            }
         }
     }
-}
 }
