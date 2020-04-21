df762faeab793f87eadb0a34d8c19eb81b9e730a
Adam Plocher <aplocher@varis1.com>
2020-04-21 10:42:35 -0700

    Initial

---
+++ WpfApp3/App.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    { 
+        public App()
+        {
+            DispatcherUnhandledException += (sender, args) =>
+            {
+                Console.WriteLine("Test7");
+            };
+
+            TaskScheduler.UnobservedTaskException += (sender, args) =>
+            {
+                Console.WriteLine("Test7");
+            };
+        }
+
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            // Process unhandled exception
+            Console.WriteLine("Test6");
+
+
+            // Prevent default unhandled exception processing
+            e.Handled = true;
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            this.Dispatcher.UnhandledException += (sender, args) =>
+            {
+                Console.WriteLine("Test1");
+            };
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+            {
+                Console.WriteLine("Test2");
+            };
+
+            Application.Current.DispatcherUnhandledException += (sender, args) =>
+            {
+                Console.WriteLine("Test3");
+            };
+
+            AppDomain.CurrentDomain.FirstChanceException += (sender, args) =>
+            {
+                Console.WriteLine("Test4");
+            };
+
+            Application.Current.Dispatcher.UnhandledException += (sender, args) =>
+            {
+                Console.WriteLine("Test5");
+            };
+        }
+    }
+}
---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var t = new TestViewModel();
+            this.DataContext = t;
+
+            // Uncommenting either of these lines WILL cause the exception to show up in the Events in App.xaml.cs
+
+            //t.TestProp = "Hello";
+
+            //throw new Exception("Test Exceptionnnn");
+        }
+    }
+
+
+public class TestViewModel
+{
+    private string testProp;
+
+    public string TestProp
+    {
+        get => testProp;
+        set
+        {
+            testProp = value;
+            throw new Exception("Test Exceptionnnn");
+        }
+    }
+}
+}
